a738950c5221b9e1625625e48cafc5135db38141
Rodrigo2004 <rodrigo_samambaia@hotmail.com>
2026-01-18 15:14:25 +1300

    Initial commit

---
+++ Source/ObstacleAssault/MovingPlatform.cpp
@@ -0,0 +1,56 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MovingPlatform.h"
+
+// Sets default values
+AMovingPlatform::AMovingPlatform()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+// Called when the game starts or when spawned
+void AMovingPlatform::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	StartLocation = GetActorLocation();
+	
+}
+
+// Called every frame
+void AMovingPlatform::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+	
+	MovePlatform(DeltaTime);
+	
+	RotatePlatform(DeltaTime);
+	
+}
+
+void AMovingPlatform ::MovePlatform(float DeltaTime)
+{
+	FVector CurrentLocation = GetActorLocation();
+	
+	CurrentLocation = CurrentLocation+ (PlatformVelocity * DeltaTime);
+	
+	SetActorLocation(CurrentLocation);
+	
+	DistanceMove = FVector::Dist(CurrentLocation, StartLocation);
+	
+	if (DistanceMove >= MoveDistance)
+	{
+		PlatformVelocity = -PlatformVelocity;
+		StartLocation = CurrentLocation;
+	}
+}
+
+void AMovingPlatform ::RotatePlatform(float DeltaTime)
+{
+	FVector CurrentLocation = GetActorLocation();
+	CurrentLocation = CurrentLocation + (PlatformVelocity * DeltaTime);
+	SetActorLocation(CurrentLocation);
+}
+
